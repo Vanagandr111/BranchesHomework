c33690cf31943f15319b0dd045813849bf781ae1
Artur <super-puper20012@mail.ru>
2023-06-06 11:54:10 +0300

    Truncated Mean

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,10 @@ public class Main {
 
     logger.info("Продажи в данном месяце: " + Arrays.toString(salesInCurrentMonth));
     logger.info("Максимальная цена продажи в данном месяце была: " + managerCurrentMonth.max());
+    logger.info("Обрезанное среднее продаж в данном месяце: " + managerCurrentMonth.truncatedMean());
 
     logger.info("Продажи в последнем месяце: " + Arrays.toString(salesInLastMonth));
     logger.info("Максимальная цена продажи в последнем месяце была: " + managerLastMonth.max());
+    logger.info("Обрезанное среднее продаж в последнем месяце: " + managerLastMonth.truncatedMean());
   }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 
 public class SalesManager {
   protected int[] sales;
 
   public SalesManager(int[] sales) {
+    Arrays.sort(sales);
     this.sales = sales;
   }
 
@@ -14,5 +16,28 @@ public class SalesManager {
       }
     }
     return max;
+  }
+
+  public int truncatedMean() {
+    int mean = 0;
+
+    if(sales.length == 0) {
+      return -1;
+    }
+
+    for(int sale: sales) {
+      mean += sale;
+    }
+
+    if(sales.length > 3) {
+      mean -= sales[0];
+      mean -= sales[sales.length-1];
+
+      mean /= sales.length - 2;
+    } else {
+      mean /= sales.length;
+    }
+
+    return mean;
   }
 }
